dca1aa6ebab4e0bc3f58be0b2fd8f193cac4b03d
linguosity <brandon@linguosity.ai>
2024-05-17 13:17:54 -0700

    update home page with fetch for customer list to test api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,163 @@
 "use client";
 
-import { Sidebar } from "flowbite-react";
+import { Sidebar, Table } from "flowbite-react";
 import { BiBuoy } from "react-icons/bi";
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
-
+import {useEffect, useState} from 'react';
 
 export default function Home() {
+
+  //
+  // Address Interface: Corresponds to the Address model in Django
+  interface Address {
+  id: number; // This represents the primary key
+  customer: number; // ForeignKey relation to Customer
+  street: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  country: string;
+  }
+
+// Employee Interface: Corresponds to the Employee model in Django
+  interface Customer {
+  id: number; // This represents the primary key
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  }
+
+  //set customerList state to update when data fetch is complete
+  const [customerList, setCustomerList] = useState<Customer[]>([]);
+
+  const getCustomerData = async () => {
+    try{
+      const res = await fetch('http://127.0.0.1:8000/customers/');
+      // The return value is *not* serialized
+      // You can return Date, Map, Set, etc.
+     
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data');
+      }
+      
+      const customerData: Customer[] = await res.json();
+      setCustomerList(customerData);
+      console.log(customerData);
+
+    }catch(err){
+      console.log('error fetching customer data', err)
+    }
+   
+  }
+
+  useEffect(()=> {
+    getCustomerData();
+  }, []);
+
+
   return (
     <main>
-           <Sidebar aria-label="Sidebar with content separator example">
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Dashboard
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards}>
-            Kanban
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiInbox}>
-            Inbox
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
-            Users
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
-            Products
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            Sign In
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
-            Sign Up
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Upgrade to Pro
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards}>
-            Documentation
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={BiBuoy}>
-            Help
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+      <Sidebar aria-label="Sidebar with content separator example">
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item href="#" icon={HiChartPie}>
+              Dashboard
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiViewBoards}>
+              Kanban
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiInbox}>
+              Inbox
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiUser}>
+              Users
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiShoppingBag}>
+              Products
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiArrowSmRight}>
+              Sign In
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiTable}>
+              Sign Up
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item href="#" icon={HiChartPie}>
+              Upgrade to Pro
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={HiViewBoards}>
+              Documentation
+            </Sidebar.Item>
+            <Sidebar.Item href="#" icon={BiBuoy}>
+              Help
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+      
+    <div className="overflow-x-auto">
+      <span></span>
+      <Table>
+        <Table.Head>
+        {customerList.map((customer, idx)=> {
+                    return(
+                      <li key={idx}>
+                        {customer.first_name};
+                      </li>
+                    )
+        })}
+          <Table.HeadCell>Product name</Table.HeadCell>
+          <Table.HeadCell>Color</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>
+            <span className="sr-only">Edit</span>
+          </Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Apple MacBook Pro 17"'}
+            </Table.Cell>
+            <Table.Cell>Sliver</Table.Cell>
+            <Table.Cell>Laptop</Table.Cell>
+            <Table.Cell>$2999</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Microsoft Surface Pro
+            </Table.Cell>
+            <Table.Cell>White</Table.Cell>
+            <Table.Cell>Laptop PC</Table.Cell>
+            <Table.Cell>$1999</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+            <Table.Cell>Black</Table.Cell>
+            <Table.Cell>Accessories</Table.Cell>
+            <Table.Cell>$99</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
     </main>
 
 
