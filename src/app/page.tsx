8818f1eb63d1796fd3225827ada041d83ddc625e
linguosity <brandon@linguosity.ai>
2024-05-17 23:15:59 -0700

    updated global.css to import flowbite, fix table visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Sidebar, Table } from "flowbite-react";
+import { Checkbox, Sidebar, Table, Card } from "flowbite-react";
 import { BiBuoy } from "react-icons/bi";
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
 import {useEffect, useState} from 'react';
@@ -26,6 +26,7 @@ export default function Home() {
   last_name: string;
   email: string;
   phone_number: string;
+  shipping_addresses: Address[];
   }
 
   //set customerList state to update when data fetch is complete
@@ -58,108 +59,86 @@ export default function Home() {
 
 
   return (
-    <main>
-      <Sidebar aria-label="Sidebar with content separator example">
-        <Sidebar.Items>
-          <Sidebar.ItemGroup>
-            <Sidebar.Item href="#" icon={HiChartPie}>
-              Dashboard
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiViewBoards}>
-              Kanban
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiInbox}>
-              Inbox
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiUser}>
-              Users
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiShoppingBag}>
-              Products
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiArrowSmRight}>
-              Sign In
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiTable}>
-              Sign Up
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
-          <Sidebar.ItemGroup>
-            <Sidebar.Item href="#" icon={HiChartPie}>
-              Upgrade to Pro
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiViewBoards}>
-              Documentation
-            </Sidebar.Item>
-            <Sidebar.Item href="#" icon={BiBuoy}>
-              Help
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
-        </Sidebar.Items>
-      </Sidebar>
-      
-    <div className="overflow-x-auto">
-      <span></span>
-      <Table>
-        <Table.Head>
-        {customerList.map((customer, idx)=> {
-                    return(
-                      <li key={idx}>
-                        {customer.first_name};
-                      </li>
-                    )
-        })}
-          <Table.HeadCell>Product name</Table.HeadCell>
-          <Table.HeadCell>Color</Table.HeadCell>
-          <Table.HeadCell>Category</Table.HeadCell>
-          <Table.HeadCell>Price</Table.HeadCell>
-          <Table.HeadCell>
-            <span className="sr-only">Edit</span>
-          </Table.HeadCell>
-        </Table.Head>
-        <Table.Body className="divide-y">
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
-            </Table.Cell>
-            <Table.Cell>Sliver</Table.Cell>
-            <Table.Cell>Laptop</Table.Cell>
-            <Table.Cell>$2999</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Microsoft Surface Pro
-            </Table.Cell>
-            <Table.Cell>White</Table.Cell>
-            <Table.Cell>Laptop PC</Table.Cell>
-            <Table.Cell>$1999</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
-            <Table.Cell>Black</Table.Cell>
-            <Table.Cell>Accessories</Table.Cell>
-            <Table.Cell>$99</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+    <>
+      <aside>
+        <Sidebar id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+          <Sidebar.Items className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <Sidebar.ItemGroup className="space-y-2 font-medium">
+              <Sidebar.Item href="#" icon={HiChartPie} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Active Orders
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox}>
+                Frames
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Prints
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Scans
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Customers
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Upgrade to Pro
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Documentation
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BiBuoy}>
+                Help
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      </aside>
+
+      <div className="p-4 sm:ml-64">
+      <Card className="flex flex-col w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
+        <div className="overflow-x-auto">
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>
+                <Checkbox />
+              </Table.HeadCell>
+              <Table.HeadCell>Last name</Table.HeadCell>
+              <Table.HeadCell>First name</Table.HeadCell>
+              <Table.HeadCell>Email</Table.HeadCell>
+              <Table.HeadCell>Phone</Table.HeadCell>
+              <Table.HeadCell>Street</Table.HeadCell>
+              <Table.HeadCell>City</Table.HeadCell>
+              <Table.HeadCell>Zip</Table.HeadCell>
+              <Table.HeadCell>Country</Table.HeadCell>
+            </Table.Head>
+            <Table.Body>
+              {customerList.map((customer, idx) => (
+                <Table.Row key={idx}>
+                  <Table.Cell>
+                    <Checkbox />
+                  </Table.Cell>
+                  <Table.Cell>{customer.last_name}</Table.Cell>
+                  <Table.Cell>{customer.first_name}</Table.Cell>
+                  <Table.Cell>{customer.email}</Table.Cell>
+                  <Table.Cell>{customer.phone_number}</Table.Cell>
+                  <Table.Cell>{customer.shipping_addresses[0].street}</Table.Cell>
+                  <Table.Cell>{customer.shipping_addresses[0].city}</Table.Cell>
+                  <Table.Cell>{customer.shipping_addresses[0].zip_code}</Table.Cell>
+                  <Table.Cell>{customer.shipping_addresses[0].country}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      </Card>
     </div>
-    </main>
-
-
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,16 +9,79 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    
+      "root": {
+        "base": "w-full text-left text-sm text-gray-500 dark:text-gray-400",
+        "shadow": "absolute left-0 top-0 -z-10 h-full w-full rounded-lg bg-white drop-shadow-md dark:bg-black",
+        "wrapper": "relative"
+      },
+      "body": {
+        "base": "group/body",
+        "cell": {
+          "base": "px-6 py-4 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg"
+        }
+      },
+      "head": {
+        "base": "group/head text-xs uppercase text-gray-700 dark:text-gray-400",
+        "cell": {
+          "base": "bg-gray-50 px-6 py-3 group-first/head:first:rounded-tl-lg group-first/head:last:rounded-tr-lg dark:bg-gray-700"
+        }
+      },
+      "row": {
+        "base": "group/row",
+        "hovered": "hover:bg-gray-50 dark:hover:bg-gray-600",
+        "striped": "odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700"
+      },
+  
     extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
   ],
 };
 export default config;
