8952fa65676de24c1c7e0bcdd754de3518318835
linguosity <brandon@linguosity.ai>
2024-05-31 23:18:50 -0700

    modify customer op and edit customer files

--- src/app/api/customerOperations.ts
+++ src/app/api/customerOperations.ts
@@ -3,6 +3,9 @@
 import { Customer } from '../types/customer';
 import { useRouter } from 'next/navigation';
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
+
 export const createCustomer = async (form: Customer, router: any) => {
     const URL = `${process.env.NEXT_PUBLIC_API_URL}/customers/add_customer/`;
 
@@ -28,4 +31,25 @@ export const createCustomer = async (form: Customer, router: any) => {
         console.error('Failed to create the customer:', error);
         throw error;
     }
+};
+
+
+export const updateCustomer = async (form: Customer, router: any) => {
+    const response = await fetch(`${API_URL}/customers/${form.id}/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${API_TOKEN}`
+        },
+        credentials: 'include',
+        body: JSON.stringify(form),
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    router.push('/'); // Navigate to the customer list or wherever necessary
+    return data;
 };
--- src/app/components/EditCustomer.tsx
+++ src/app/components/EditCustomer.tsx
@@ -1,51 +1,140 @@
 'use client';
 
-import { Card } from 'flowbite-react';
-import Image from 'next/image';
-import { Customer } from '../types/customer';
-import { Label, TextInput } from "flowbite-react";
+import { Card, Label, TextInput, Button, Select } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Customer, Address } from '../types/customer';
+import { updateCustomer } from '../api/customerOperations';
+import { useRouter } from 'next/navigation';
+import { HiMail } from 'react-icons/hi';
 
 interface EditCustomerProps {
     customerData: Customer;
 }
 
+const usStates = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 
+    'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 
+    'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
+    'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 
+    'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
+    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
 const EditCustomer: React.FC<EditCustomerProps> = ({ customerData }) => {
+    const [form, setForm] = useState<Customer>(customerData);
+    const router = useRouter();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value, type } = e.target;
+        const newValue = type === 'checkbox' ? (e.target as HTMLInputElement).checked : value;
+
+        if (['street', 'city', 'state', 'zip_code', 'country'].includes(name)) {
+            setForm(prevForm => ({
+                ...prevForm,
+                shipping_addresses: [{ ...prevForm.shipping_addresses[0], [name]: newValue }]
+            }));
+        } else {
+            setForm(prevForm => ({
+                ...prevForm,
+                [name]: newValue
+            }));
+        }
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (form) {
+            try {
+                console.log('Submitting form data:', JSON.stringify(form, null, 2));
+                await updateCustomer(form, router);
+            } catch (error) {
+                console.error('Failed to update the customer:', error);
+            }
+        }
+    };
+
     return (
-        <Card className="max-w-sm">
+        <Card className="size-full">
             <div className="mb-4 flex items-center justify-between">
                 <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white"> Edit Customer Details</h5>
             </div>
 
-            <div className="flow-root">
+            <form className="flow-root" onSubmit={handleSubmit}>
                 <div className="flex max-w-md flex-col gap-4">
                     <div>
                         <div className="mb-2 block">
-                        <Label htmlFor="small" value="First name" />
+                            <Label htmlFor="first_name" value="First name" />
                         </div>
-                        <TextInput id="small" type="text" sizing="sm" value={customerData.first_name}/>
+                        <TextInput id="first_name" name="first_name" type="text" sizing="sm" value={form.first_name} onChange={handleChange} required />
                     </div>
                     <div>
                         <div className="mb-2 block">
-                        <Label htmlFor="small" value="First name" />
+                            <Label htmlFor="last_name" value="Last name" />
                         </div>
-                        <TextInput id="small" type="text" sizing="sm" value={customerData.last_name}/>
+                        <TextInput id="last_name" name="last_name" type="text" sizing="sm" value={form.last_name} onChange={handleChange} required />
                     </div>
                     <div>
                         <div className="mb-2 block">
-                        <Label htmlFor="email2" value="Email" />
+                            <Label htmlFor="email" value="Email" />
                         </div>
-                        <TextInput id="email2" type="email" placeholder={customerData.email} required shadow />
+                        <TextInput id="email" name="email" type="email" placeholder="name@flowbite.com" icon={HiMail} value={form.email} onChange={handleChange} required />
                     </div>
                     <div>
                         <div className="mb-2 block">
-                        <Label htmlFor="small" value="Phone number" />
+                            <Label htmlFor="phone_number" value="Phone number" />
                         </div>
-                        <TextInput id="small" type="text" sizing="sm" value={customerData.phone_number}/>
+                        <TextInput id="phone_number" name="phone_number" type="text" sizing="sm" value={form.phone_number} onChange={handleChange} required />
                     </div>
+                    {/* Street */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="street" value="Street" />
+                        </div>
+                        <TextInput name="street" id="street" placeholder="" required value={form.shipping_addresses[0]?.street || ''} onChange={handleChange} />
+                    </div>
+                    {/* City */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="city" value="City" />
+                        </div>
+                        <TextInput name="city" id="city" placeholder="" required value={form.shipping_addresses[0]?.city || ''} onChange={handleChange} />
+                    </div>
+                    {/* State */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="state" value="Select your state" />
+                        </div>
+                        <Select id="state" name="state" required value={form.shipping_addresses[0]?.state || ''} onChange={handleChange}>
+                            <option value="" disabled>Select a state</option>
+                            {usStates.map(state => (
+                                <option key={state} value={state}>{state}</option>
+                            ))}
+                        </Select>
+                    </div>
+                    {/* Zip Code */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="zip_code" value="Zip Code" />
+                        </div>
+                        <TextInput id="zip_code" name="zip_code" placeholder="" required value={form.shipping_addresses[0]?.zip_code || ''} onChange={handleChange} />
+                    </div>
+                    {/* Country */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="country" value="Select your country" />
+                        </div>
+                        <Select id="country" name="country" required value={form.shipping_addresses[0]?.country || ''} onChange={handleChange}>
+                            <option value="" disabled>Select a country</option>
+                            <option>United States</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                        </Select>
+                    </div>
+                    <Button type="submit" color="blue">Submit</Button>
                 </div>
-            </div>
+            </form>
         </Card>
-        
     );
 };
 
